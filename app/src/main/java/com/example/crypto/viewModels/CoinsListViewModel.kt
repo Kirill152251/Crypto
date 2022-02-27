ad5d95f4af7e560c57e03c8d722f1f4e76beae61
Your Name <kirill.ermalenok2001@gmail.com>
2022-02-27 15:41:00 +0300

    add pull to refresh

--- app/src/main/java/com/example/crypto/viewModels/CoinsListViewModel.kt
+++ app/src/main/java/com/example/crypto/viewModels/CoinsListViewModel.kt
@@ -5,6 +5,9 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.crypto.model.api.responses.coinsList.Coin
+import com.example.crypto.model.constans.QUERY_SORT_BY_MARKET_CAP
+import com.example.crypto.model.constans.QUERY_SORT_BY_PRICE
+import com.example.crypto.model.constans.QUERY_SORT_BY_VOLATILITY
 import com.example.crypto.repository.CoinsListRepository
 import kotlinx.coroutines.flow.*
 
@@ -12,8 +15,26 @@ class CoinsListViewModel(
     private val repository: CoinsListRepository
 ) : ViewModel() {
 
+    private val _currentSorting = MutableStateFlow(QUERY_SORT_BY_MARKET_CAP)
+    val currentSorting: StateFlow<String> = _currentSorting
+
     @OptIn(ExperimentalPagingApi::class)
     fun getCoins(order: String): Flow<PagingData<Coin>> =
         repository.getCoinsListFlow(order).cachedIn(viewModelScope)
+
+    fun getCoinsByMarketCap(): Flow<PagingData<Coin>> {
+        _currentSorting.value = QUERY_SORT_BY_MARKET_CAP
+        return  repository.getCoinsByCap().cachedIn(viewModelScope)
+    }
+
+    fun getCoinsByPrice(): Flow<PagingData<Coin>> {
+        _currentSorting.value = QUERY_SORT_BY_PRICE
+        return  repository.getCoinsByPrice().cachedIn(viewModelScope)
+    }
+
+    fun getCoinsByVolatility(): Flow<PagingData<Coin>> {
+        _currentSorting.value = QUERY_SORT_BY_VOLATILITY
+        return  repository.getCoinsByVol().cachedIn(viewModelScope)
+    }
 }
 
